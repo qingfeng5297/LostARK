d389907569fdefff19cb62b7516e719b33299256
qingfeng5297 <54947503+qingfeng5297@users.noreply.github.com>
2022-06-12 12:58:45 +0800

    Update socks5.sh

--- socks5.sh
+++ socks5.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-yum install gcc openldap-devel pam-devel openssl-devel -y && yum install -y wget && systemctl stop firewalld.service && wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh -O install.sh && bash install.sh  --port=8888 --user=123 --passwd=123
+
+yum install gcc openldap-devel pam-devel openssl-devel -y 
+yum install -y wget && systemctl stop firewalld.service
+wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh -O install.sh 
+bash install.sh  --port=8888 --user=123 --passwd=123
